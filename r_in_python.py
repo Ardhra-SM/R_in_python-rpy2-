a6564c79def8f3e13bd4ee2680602b9004966585
Ardhra Madhavan <137415391+Ardhra-SM@users.noreply.github.com>
2024-01-17 14:03:19 +0100

    Update r_in_python.py

--- r_in_python.py
+++ r_in_python.py
@@ -1,6 +1,9 @@
+# On Windows, from a command prompt use the following command to set the R_HOME permanently as a user environment variable, replacing C:\rpath\bin with your R install location:
+# setx R_HOME "C:\rpath\bin" [This can be typed into any command prompt and then the code that follows below can be written before any R package is called in python.]
+
 import os
 os.environ["R_HOME"] = r"C:/Program Files/R/R-4.3.2"
-os.environ["PATH"]   = r"C:/Program Files/R/R-4.3.2/bin/x64" + ";" + os.environ["PATH"]
+os.environ["PATH"]   = r"C:/Program Files/R/R-4.3.2/bin/x64" + ";" + os.environ["PATH"] # This the fix for pointing the R_HOME file after it has been created.
 
 # Import necessary packages:
 import rpy2.robjects as robjects
